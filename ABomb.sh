8653d799ba6bb5830d97d4845b477fcae3659265
ADIL <90197148+IND-GANG@users.noreply.github.com>
2021-09-23 07:51:31 +0530

    Add files via upload

--- ABomb.sh
+++ ABomb.sh
@@ -20,8 +20,6 @@ detect_distro() {
     fi
     figlet LOADING....
     apt-get install sox -y
-# Loading spinner
-    source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
     play .granted.mp3
 }
 banner() {
@@ -132,7 +130,8 @@ do
     figlet EXIT....
         exit
     else
-        (echo -e "\e[4;32m404 ERROR\e[0m" $play .DENIED.mp3 )
+        (echo -e "\e[4;32m404 ERROR\e[0m")
+        play .denied.mp3
         exit
         pause
     fi
